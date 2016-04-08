8680cff7277a66cd700aa5b7bdb4747d2ddec3be
Herman <officialzhanghang@gmail.com>
2016-04-08 22:31:18 +0800

    Add Limiter

--- src/main/java/octoteam/tahiti/quota/CapacityLimiter.java
+++ src/main/java/octoteam/tahiti/quota/CapacityLimiter.java
@@ -1,35 +1,43 @@
 package octoteam.tahiti.quota;
 
 /**
- * TODO
+ * 基于容量的限流器。获得令牌超过允许容量阈值限制后，就停止提供服务.
  */
 public class CapacityLimiter extends QuotaLimiter {
 
+    private final int capacity;
+    private int acquired = 0;
+
     /**
-     * TODO
+     * 根据传入数值设置容量阈值
      *
-     * @param capacity
+     * @param capacity int 容量阈值
      */
     public CapacityLimiter(int capacity) {
-
+        this.capacity = capacity;
     }
 
     /**
-     * TODO
+     * 尝试获取令牌,判断是否到达容量阈值.
      *
-     * @return
+     * @return 如果未超出阈值返回 true, 否则返回 false.
      */
     public boolean tryAcquire() {
-        return false;
+        if (acquired < capacity) {
+            acquired++;
+            return true;
+        } else {
+            return false;
+        }
     }
 
     /**
-     * TODO
+     * 获取预设的容量阈值.
      *
-     * @return
+     * @return 返回预设的容量阈值.
      */
     public int getCapacity() {
-        return 0;
+        return this.capacity;
     }
 
 }
--- src/main/java/octoteam/tahiti/quota/QuotaLimiter.java
+++ src/main/java/octoteam/tahiti/quota/QuotaLimiter.java
@@ -1,14 +1,15 @@
 package octoteam.tahiti.quota;
 
 /**
- * TODO
+ * 所有类型的 Limiter 都应该继承该抽象类, 使用 tryAcquire() 判断是否到达阈值,
+ * 基于已收到的消息数量和预设的阈值来判断是否继续提供服务.
  */
 public abstract class QuotaLimiter {
 
     /**
-     * TODO
+     * 尝试获取令牌,判断是否到达阈值
      *
-     * @return
+     * @return 如果未超出阈值返回 true，否则返回 false。
      */
     public abstract boolean tryAcquire();
 
