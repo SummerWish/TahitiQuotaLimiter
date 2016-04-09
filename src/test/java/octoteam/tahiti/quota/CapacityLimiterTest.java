c1fa45ab45f999883d3fc0e7666d95fb12fc1288
Breezewish <me@breeswish.org>
2016-04-09 20:03:11 +0800

    getCapacity

--- src/test/java/octoteam/tahiti/quota/CapacityLimiterTest.java
+++ src/test/java/octoteam/tahiti/quota/CapacityLimiterTest.java
@@ -9,6 +9,7 @@ public class CapacityLimiterTest {
     @Test
     public void testCapacityLimiter() {
         CapacityLimiter limiter = new CapacityLimiter(4);
+        assertEquals(4, limiter.getCapacity());
         assertTrue(limiter.tryAcquire());
         assertTrue(limiter.tryAcquire());
         assertTrue(limiter.tryAcquire());
